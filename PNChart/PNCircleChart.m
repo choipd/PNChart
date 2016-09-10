063d434351fabba3182165d03f1a7c76573e7bdf
choipd <choipd.net@gmail.com>
2016-09-10 19:41:37 +0900

    https://github.com/kevinzhow/PNChart/issues/321

--- PNChart/PNCircleChart.m
+++ PNChart/PNCircleChart.m
@@ -210,9 +210,23 @@ displayCountingLabel:(BOOL)displayCountingLabel
 
 -(void)updateChartByCurrent:(NSNumber *)current byTotal:(NSNumber *)total {
     double totalPercentageValue = [current floatValue]/([total floatValue]/100.0);
+    float previousStrokeEnd = [_current floatValue] / [_total floatValue];
+    float currentStrokeEnd = [current floatValue] / [total floatValue];
+    
+    
+    // when currentValue is less than or equal to previousValue
+    if ((fabs(currentStrokeEnd) < fabs(previousStrokeEnd) + FLT_EPSILON) ||
+        (fabs((currentStrokeEnd) - (previousStrokeEnd)) < FLT_EPSILON))
+    {
+        _circle.strokeEnd = currentStrokeEnd;
+    }
+    // when currentValue is larger than previousValue
+    else {
+        _circle.strokeEnd = previousStrokeEnd;
+    }
     
     if (_strokeColorGradientStart) {
-        self.gradientMask.strokeEnd = _circle.strokeEnd;
+        self.gradientMask.strokeEnd = currentStrokeEnd;
     }
     
     // Add animation
@@ -237,7 +251,7 @@ displayCountingLabel:(BOOL)displayCountingLabel
         [self.countingLabel countFrom:totalPercentageValue to:totalPercentageValue withDuration:self.duration];
     }
     
-    _circle.strokeEnd   = [current floatValue] / [total floatValue];
+//    _circle.strokeEnd   = [current floatValue] / [total floatValue];
     _current = current;
     _total = total;
 }
